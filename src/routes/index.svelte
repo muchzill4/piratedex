eec99809d120594e6d185fdffc5bf8fcb362bfc6
Bartek Mucha <muchzill4@gmail.com>
2021-07-23 19:25:31 +0100

    Format

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -55,91 +55,91 @@
 </main>
 
 <style>
-main {
-  max-width: 50rem;
-  margin: 0 auto;
-}
+  main {
+    max-width: 50rem;
+    margin: 0 auto;
+  }
 
-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 2rem;
-}
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 2rem;
+  }
 
-label {
-  font-size: var(--font-size--medium);
-  padding: 0 .5rem;
-}
+  label {
+    font-size: var(--font-size--medium);
+    padding: 0 0.5rem;
+  }
 
-input {
-  border-radius: 20rem;
-  background: #fafafa;
-  border: 0;
-  width: 12rem;
-  height: 3rem;
-  padding: 0 1rem;
-  font-size: var(--font-size--medium);
-  margin-left: .5rem;
-}
+  input {
+    border-radius: 20rem;
+    background: #fafafa;
+    border: 0;
+    width: 12rem;
+    height: 3rem;
+    padding: 0 1rem;
+    font-size: var(--font-size--medium);
+    margin-left: 0.5rem;
+  }
 
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
 
-li {
-  margin-bottom: 20px;
-}
+  li {
+    margin-bottom: 20px;
+  }
 
-article {
-  background: #fafafa;
-  color: #262626;
-  padding: 1rem;
-  border-radius: 0.2rem;
-  box-shadow: 0 5px 5px rgba(0,0,0,0.05);
-}
+  article {
+    background: #fafafa;
+    color: #262626;
+    padding: 1rem;
+    border-radius: 0.2rem;
+    box-shadow: 0 5px 5px rgba(0, 0, 0, 0.05);
+  }
 
-h2 {
-  margin-bottom: 0.5rem;
-}
+  h2 {
+    margin-bottom: 0.5rem;
+  }
 
-dl {
-  margin-bottom: 1rem;
-}
+  dl {
+    margin-bottom: 1rem;
+  }
 
-dt {
-  font-size: var(--font-size--small);
-  text-transform: lowercase;
-  display: inline-block;
-  margin-right: 0.2rem;
-  color: #666;
-}
+  dt {
+    font-size: var(--font-size--small);
+    text-transform: lowercase;
+    display: inline-block;
+    margin-right: 0.2rem;
+    color: #666;
+  }
 
-dt:after {
-  content: ':';
-}
+  dt:after {
+    content: ':';
+  }
 
-dd {
-  display: inline-block;
-  margin-right: 1rem;
-}
+  dd {
+    display: inline-block;
+    margin-right: 1rem;
+  }
 
-p {
-  font-size: var(--font-size--default);
-  padding-left: 1rem;
-  position: relative;
-}
+  p {
+    font-size: var(--font-size--default);
+    padding-left: 1rem;
+    position: relative;
+  }
 
-p:before {
-  position: absolute;
-  top: -0.5rem;
-  font-size: 1.8rem;
-  left: 0;
-  content: "“";
-  font-weight: bold;
-  color: #999;
-  font-family: 'Georgia', serif;
-}
+  p:before {
+    position: absolute;
+    top: -0.5rem;
+    font-size: 1.8rem;
+    left: 0;
+    content: '“';
+    font-weight: bold;
+    color: #999;
+    font-family: 'Georgia', serif;
+  }
 </style>
